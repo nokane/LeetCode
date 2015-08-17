675a1b5e0c0914de7a12a52eada5d70f3b501332
Niall O'Kane <okanenm@gmail.com>
2015-08-17 16:42:30 -0700

    Implement addDigits()

--- js/addDigits.js
+++ js/addDigits.js
@@ -7,5 +7,18 @@ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one
  * @return {number}
  */
 var addDigits = function(num) {
-    
+
+  var recurseNum = function(checkNum) {
+    var numString = checkNum.toString();
+    if (numString.length === 1) {
+      return checkNum;
+    }
+    var total = 0;
+    for (var i = 0; i < numString.length; i++) {
+      total += parseInt(numString.charAt(i));
+    }
+    return recurseNum(total);
+  }
+
+  return recurseNum(num);
 };
