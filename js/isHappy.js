b2cd7f88415dcde0c301e977e2df6fbe93d81de8
Niall O'Kane <okanenm@gmail.com>
2015-08-17 16:52:51 -0700

    fix comment

--- js/isHappy.js
+++ js/isHappy.js
@@ -6,14 +6,15 @@ A happy number is a number defined by the following process: Starting with any p
 
 Example: 19 is a happy number
 
-12 + 92 = 82
-82 + 22 = 68
-62 + 82 = 100
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
 
  * @param {number} n
  * @return {boolean}
  */
 
 var isHappy = function(n) {
-    
+  
 };
