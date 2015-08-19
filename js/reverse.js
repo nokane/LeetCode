d635bb93ff46cdcaa08bf71d3b2fd8158ae654b5
Niall O'Kane <okanenm@gmail.com>
2015-08-18 19:25:22 -0700

    Implement reverse()

--- js/reverse.js
+++ js/reverse.js
@@ -7,6 +7,19 @@ Example2: x = -123, return -321
  * @param {number} x
  * @return {number}
  */
+
 var reverse = function(x) {
-    
+  var reversed = '';
+  if (x < 0) {
+    x *= -1;
+    reversed += '-';
+  }
+  var stringified = x.toString();
+
+  for (var i = stringified.length - 1; i >= 0; i--) {
+    reversed += stringified.charAt(i);
+  }
+
+  return parseInt(reversed);
+
 };
