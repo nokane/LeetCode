86009e390ea7d4b67a5a68601c9caaeeca3ca6d9
Niall O'Kane <okanenm@gmail.com>
2015-08-22 13:55:38 -0700

    Initialize longestPalindrome()

---
+++ py/longestPalindrome.py
@@ -0,0 +1,10 @@
+"""
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+"""
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
