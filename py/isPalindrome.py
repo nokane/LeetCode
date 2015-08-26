7cd982b64e985ce2af475da5f9ad4f91d0b5a54d
Niall O'Kane <okanenm@gmail.com>
2015-08-25 19:20:19 -0700

    initialize isPalindrome()

---
+++ py/isPalindrome.py
@@ -0,0 +1,9 @@
+"""
+Determine whether an integer is a palindrome. Do this without extra space.
+"""
+
+def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
