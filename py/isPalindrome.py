428f4f03e457f6471191347f11165a18c9371de5
Niall O'Kane <okanenm@gmail.com>
2015-08-25 19:53:16 -0700

    Complete isPalindrome()

--- py/isPalindrome.py
+++ py/isPalindrome.py
@@ -2,8 +2,19 @@
 Determine whether an integer is a palindrome. Do this without extra space.
 """
 
-def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+def isPalindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if (x == 0):
+        return True
+    elif (x < 0) or (x % 10 == 0):
+        return False    
+    y = 0
+    while (x > y):
+        y = (y * 10) + (x % 10)
+        if (x == y):
+            return True
+        x = int(x / 10)
+    return x == y
