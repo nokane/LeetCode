756c8acdd41002f935ed66d53be4457700485068
Niall O'Kane <okanenm@gmail.com>
2015-08-27 15:57:40 -0700

    implement isValid()

--- py/validParentheses.py
+++ py/validParentheses.py
@@ -9,3 +9,38 @@ def isValid(s):
     :type s: str
     :rtype: bool
     """
+    curr = None
+    brackets = []
+    def newCurr():
+        if len(brackets) > 0:
+            return brackets.pop(len(brackets)-1)
+        else:
+            return None
+    for x in s:
+        if (x == '(') or (x == '{') or (x == '['):
+            if curr != None:
+                brackets.append(curr)
+            curr = x
+        elif x == ')':
+            if curr != '(':
+                return False
+            else:
+                curr = newCurr()
+                print(curr)
+        elif x == '}':
+            if curr != '{':
+                return False
+            else:
+                curr = newCurr()
+                print(curr)
+        elif x == ']':
+            if curr != '[':
+                return False
+            else:
+                curr = newCurr()
+                print(curr)
+    if (len(brackets) > 0) or (curr != None):
+        return False
+    else:
+        return True
+
