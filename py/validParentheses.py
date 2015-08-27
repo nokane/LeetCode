fd10a0a74a2ee2999ed36702a69b008a6c2af0dd
Niall O'Kane <okanenm@gmail.com>
2015-08-27 14:41:00 -0700

    Initialize validParentheses

---
+++ py/validParentheses.py
@@ -0,0 +1,11 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+"""
+
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
