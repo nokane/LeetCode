1fdd6ac1160b2949ab282b02bf75a8a5e00a3aa3
Niall O'Kane <okanenm@gmail.com>
2015-09-02 15:54:37 -0700

    Implemented maxArea() used to determine biggest container

--- py/biggestContainer.py
+++ py/biggestContainer.py
@@ -10,3 +10,15 @@ def maxArea(height):
     :type height: List[int]
     :rtype: int
     """
+    if (len(height) == 0) or (len(height) == 1):
+        return 0
+    left = 0
+    right = len(height) - 1
+    bigArea = 0
+    while left < right:
+        bigArea = max(bigArea, min(height[left], height[right])*(right - left))
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return bigArea
