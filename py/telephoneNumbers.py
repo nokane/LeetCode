167a1daa7713d31209b70ff2229bc90513e7c871
Niall O'Kane <okanenm@gmail.com>
2015-09-02 14:58:15 -0700

    Implemented letterCombinations() - telephoneNumbers

--- py/telephoneNumbers.py
+++ py/telephoneNumbers.py
@@ -14,3 +14,29 @@ def letterCombinations(digits):
     :type digits: str
     :rtype: List[str]
     """
+    if digits == "":
+        return []
+    keypad = {
+      '0': '0',
+      '1': '1',
+      '2': 'abc',
+      '3': 'def',
+      '4': 'ghi',
+      '5': 'jkl',
+      '6': 'mno',
+      '7': 'pqrs',
+      '8': 'tuv',
+      '9': 'wxyz'
+    }
+    results = []
+    def recurseDigits(dig, word):
+        if len(dig) == 0:
+            results.append(word)
+            return
+        else:
+            for c in keypad[dig[0]]:
+                word += c
+                recurseDigits(dig[1:], word)
+                word = word[:-1]
+    recurseDigits(digits,'')
+    return results
