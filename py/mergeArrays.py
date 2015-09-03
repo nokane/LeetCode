2e0cd57adb4182b530ad0d891677b7d07e842bd7
Niall O'Kane <okanenm@gmail.com>
2015-09-02 17:03:30 -0700

    Implemented merge() - which allows the user to merge two sorted arrays

--- py/mergeArrays.py
+++ py/mergeArrays.py
@@ -6,7 +6,10 @@ Note:
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 """
 
-def merge(self, nums1, m, nums2, n):
+
+def merge(nums1, m, nums2, n):
+    if n == 0:
+        return
     """
     :type nums1: List[int]
     :type m: int
@@ -14,3 +17,27 @@ def merge(self, nums1, m, nums2, n):
     :type n: int
     :rtype: void Do not return anything, modify nums1 in-place instead.
     """
+    indexNums1 = 0
+    mIndex = n 
+    nIndex = 0
+    place = 0
+    for counter in range(m - 1, -1, -1):
+        nums1[m + n - 1 - ((m-1) - counter)] = nums1[counter]
+        nums1[counter] = None
+
+    while (nIndex < n) and (mIndex < m + n):
+        if nums1[mIndex] <= nums2[nIndex]:
+            nums1[place] = nums1[mIndex]
+            mIndex += 1
+        else:
+            nums1[place] = nums2[nIndex] 
+            nIndex += 1
+        place += 1
+    
+    while (nIndex < n):
+        nums1[place] = nums2[nIndex]
+        nIndex += 1
+        place += 1
+    
+    
+
