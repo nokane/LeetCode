c44a1d6f899a98f3f7e6b7c022870c31fa7b875b
Niall O'Kane <okanenm@gmail.com>
2015-09-03 14:52:06 -0700

    Implement zigZagConversion()

--- py/zigZagConversion.py
+++ py/zigZagConversion.py
@@ -9,6 +9,8 @@ Y   I   R
 And then read line by line: "PAHNAPLSIIGYIR"
 Write the code that will take a string and make this conversion given a number of rows:
 
+
+
 string convert(string text, int nRows);
 convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 """
@@ -19,3 +21,14 @@ def convert(s, numRows):
     :type numRows: int
     :rtype: str
     """
+    if numRows == 1:
+        return s
+    resultString = ""
+    delt = numRows * 2 - 2
+    for i in xrange(numRows):
+        for position in range(i,len(s), delt):
+            resultString += s[position]
+            if (i != 0) and (i != numRows - 1):
+                if (position + delt - 2 * i) < len(s):
+                    resultString += s[position + delt - 2 * i]
+    return resultString
