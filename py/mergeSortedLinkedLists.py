046672d79706aadd322a6f92b0307597b3aae78b
Niall O'Kane <okanenm@gmail.com>
2015-09-03 10:38:14 -0700

    Initialize mergeTwoLists()

---
+++ py/mergeSortedLinkedLists.py
@@ -0,0 +1,12 @@
+"""
+Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+"""
+
+def mergeTwoLists(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
