bc5c120a8113165581cdcda29e5eb0d69bb7e180
Niall O'Kane <okanenm@gmail.com>
2015-09-03 10:19:44 -0700

    Implemented removeNthFromEnd()

--- py/removeNthFromEnd.py
+++ py/removeNthFromEnd.py
@@ -1,5 +1,5 @@
 """
-Remove Nth Node From End of List Total Accepted: 69580 Total Submissions: 259549 My Submissions Question Solution 
+Remove Nth Node From End of List
 Given a linked list, remove the nth node from the end of list and return its head.
 
 For example,
@@ -18,3 +18,19 @@ def removeNthFromEnd(head, n):
     :type n: int
     :rtype: ListNode
     """
+    storage = []
+    currentNode = head
+    while currentNode:
+        storage.append(currentNode)
+        currentNode = currentNode.next
+    if (len(storage) == 1) and (n == 1):
+        return None
+    elif n == 1:
+        storage[len(storage)-2].next = None
+    elif len(storage) == n:
+        head = head.next
+    else:
+        storage[len(storage)-(n + 1)].next = storage[len(storage)- (n - 1)]
+    return head
+
+
