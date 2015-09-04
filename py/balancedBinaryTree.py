dadfbe6bdf62e9b57cab8abae1aa277a500cc755
Niall O'Kane <okanenm@gmail.com>
2015-09-03 17:17:39 -0700

    Implement isBalanced(), to check if Binary Tree is Balanced

--- py/balancedBinaryTree.py
+++ py/balancedBinaryTree.py
@@ -16,3 +16,22 @@ def isBalanced(root):
     :type root: TreeNode
     :rtype: bool
     """
+    if root is None:
+        return True
+    if checkHeight(root) == -1:
+        return False
+    else:
+        return True
+
+def checkHeight(node):
+    if node is None:
+        return 0
+    leftHeight = checkHeight(node.left)
+    if leftHeight == -1:
+        return -1
+    rightHeight = checkHeight(node.right)
+    if rightHead == -1:
+        return -1
+    if abs(leftHeight - rightHeight) > 1:
+        return -1
+    return max(leftHeight, rightHeight) + 1
