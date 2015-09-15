ec947c00be5e0c7f8e962fee7d77451d6e192fce
Niall O'Kane <okanenm@gmail.com>
2015-09-15 15:10:44 -0700

    Implement generateParentheses()

--- py/generateParentheses.py
+++ py/generateParentheses.py
@@ -7,8 +7,20 @@ For example, given n = 3, a solution set is:
 "((()))", "(()())", "(())()", "()(())", "()()()"
 """
 
-def generateParenthesis(n):
+def generateParentheses(n):
     """
     :type n: int
     :rtype: List[str]
     """
+    result = []
+    depthSearch(n, n, '', result)
+    return result
+
+def depthSearch(left, right, val, res):
+    if (left > right) or (left < 0) or (right < 0):
+        return
+    if (left == 0) and (right == 0):
+        res.append(val)
+        return
+    depthSearch(left - 1, right, val + '(', res)
+    depthSearch(left, right - 1, val + ')', res)
