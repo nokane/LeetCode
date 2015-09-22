46f9a3da0dc051d3872985658b8ce33ebee3be46
Niall O'Kane <okanenm@gmail.com>
2015-09-21 21:10:34 -0700

    Initialize rotateImage.py

---
+++ py/rotateImage.py
@@ -0,0 +1,17 @@
+
+"""
+Rotate Image
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+"""
+
+def rotate(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: void Do not return anything, modify matrix in-place instead.
+    """
+        
